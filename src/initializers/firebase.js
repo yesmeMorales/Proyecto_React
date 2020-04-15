6263e004a0814fed0f0b0bec7be0dc82f24eead8
Yesme <moralesyesme@gmail.com>
2020-04-14 22:43:53 -0500

    Update firebase.js

--- src/initializers/firebase.js
+++ src/initializers/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCYOYAnO_C99F-6I0DVIqnX_aFWQ_G_TJg",
-  authDomain: "album-react-9f96e.firebaseapp.com",
-  databaseURL: "https://album-react-9f96e.firebaseio.com",
-  projectId: "album-react-9f96e",
-  storageBucket: "album-react-9f96e.appspot.com",
-  messagingSenderId: "1048434255233"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
 //Con esto extraemos el resultado de la cache sin tener que volver a ejecutar lo anterior
